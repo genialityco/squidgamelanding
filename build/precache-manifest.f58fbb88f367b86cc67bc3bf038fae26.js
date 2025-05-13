531d3e2945c09e9cf3cccc47d996b945090b988e
Cristian Cano López <ccl01_29@hotmail.com>
2025-05-13 15:39:40 -0500

    build

---
+++ build/precache-manifest.f58fbb88f367b86cc67bc3bf038fae26.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91fa2a392627109deebc4dff3a29ccd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cbb44ada8936739673c",
+    "url": "/static/css/main.9b7205af.chunk.css"
+  },
+  {
+    "revision": "77813b763a6fd00495d4",
+    "url": "/static/js/2.d36a4034.chunk.js"
+  },
+  {
+    "revision": "2d606b14baae2c79b95384447ddf3b29",
+    "url": "/static/js/2.d36a4034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cbb44ada8936739673c",
+    "url": "/static/js/main.ac5bbc5e.chunk.js"
+  },
+  {
+    "revision": "6a94446c57037e981d4e",
+    "url": "/static/js/runtime-main.d9f3ccfb.js"
+  },
+  {
+    "revision": "5b8de4df6e77a901a99b3a5928891433",
+    "url": "/static/media/Game Of Squids.5b8de4df.ttf"
+  },
+  {
+    "revision": "92db132f4d6bc0efc709646eba124b8d",
+    "url": "/static/media/Game Of Squids.92db132f.otf"
+  },
+  {
+    "revision": "ea343e8149c4a91f438b99020ae46214",
+    "url": "/static/media/INSTRUCCIONES_2.ea343e81.jpg"
+  },
+  {
+    "revision": "53353f7d2a1f3991fd85d1b07b847f0d",
+    "url": "/static/media/INSTRUCCIONES_V_MOBILE.2.53353f7d.png"
+  },
+  {
+    "revision": "e110b36ac309906e372f8822898dc7d3",
+    "url": "/static/media/MONTECHV02-Regular.e110b36a.ttf"
+  },
+  {
+    "revision": "c6dba63e7331d084d47aff002f51d412",
+    "url": "/static/media/SLIDER_INTERNO.c6dba63e.jpg"
+  },
+  {
+    "revision": "8a079740c189ea0727eabedff0b486c6",
+    "url": "/static/media/SLIDER_SQUID.8a079740.jpg"
+  }
+]);
--- src/components/header/HeaderTwo.jsx
+++ src/components/header/HeaderTwo.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importa Link
+import { Link } from 'react-router-dom';
 
 export default function HeaderTwo() {
   return (
     <div className="main-banner">
       <div className="header-box-glitch">
         <div className="logo-left">
-           <a href="/"></a>
-          <img
-            src="/assets/images/LOGO_SUPERVIVENCIA_LEBIC.png"
-            alt="Logo Supervivencia Lebic"
-            className="logo-img glitch"
-          />
+          <Link to="/">
+            <img
+              src="/assets/images/LOGO_SUPERVIVENCIA_LEBIC.png"
+              alt="Logo Supervivencia Lebic"
+              className="logo-img glitch"
+            />
+          </Link>
         </div>
         <div className="button-right">
           <a href="#niveles" className="btn-niveles">NIVELES</a>
